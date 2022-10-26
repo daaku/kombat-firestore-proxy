ba0661e2e84bd0d377b6ab697ca8903c061d18b5
Naitik Shah <n@daaku.org>
2022-10-26 08:04:25 +0400

    lots of progress

--- src/index.ts
+++ src/index.ts
@@ -25,35 +25,237 @@ export interface Store<DB extends object> {
   listenChanges: () => void
 }
 
+const isPrimitive = (v: any) => {
+  if (typeof v === 'object') {
+    return v === null
+  }
+  return typeof v !== 'function'
+}
+
+// TODO: fixme
+const deepEqual = (a: any, b: any) => {
+  return a === b
+}
+
 class DBProxy {
   #s: S<any>
   constructor(s: S<any>) {
     this.#s = s
   }
-  get(_: any, p: string) {
-    return this.#s.dataset(p)
+  get(_: any, dataset: string) {
+    return this.#s.datasetProxy(dataset)
   }
   set(): any {
-    throw new TypeError('cannot set on store')
+    throw new TypeError('cannot set on DB')
+  }
+  deleteProperty(): any {
+    throw new TypeError('cannot delete on DB')
+  }
+  ownKeys() {
+    return Object.keys(this.#s.mem)
+  }
+  has(_: any, dataset: string) {
+    const mem = this.#s.mem
+    return mem && dataset in this.#s.mem
+  }
+  defineProperty(): any {
+    throw new TypeError('cannot defineProperty on DB')
+  }
+  getOwnPropertyDescriptor(_: any, p: string) {
+    return {
+      value: this.#s.datasetProxy(p),
+      writable: true,
+      enumerable: true,
+      configurable: true,
+    }
+  }
+}
+
+class RowProxy {
+  #s: S<any>
+  #dataset: string
+  #id: string
+  constructor(s: S<any>, dataset: string, id: string) {
+    this.#s = s
+    this.#dataset = dataset
+    this.#id = id
+  }
+  get(_: any, prop: string) {
+    const row = this.#s.mem[this.#dataset][this.#id]
+    const val = row[prop]
+    // hasOwn allows pass-thru of prototype properties like constructor
+    if (isPrimitive(val) || !Object.hasOwn(row, prop)) {
+      return val
+    }
+    console.log(this.#s.mem[this.#dataset][this.#id])
+    throw new Error(
+      `non primitive value for dataset "${this.#dataset}" row with id "${
+        this.#id
+      }" and property "${prop}" of type "${typeof val}" and value "${val}"`,
+    )
+  }
+  set(_: any, prop: string, value: any): any {
+    this.#s.mem[this.#dataset][this.#id][prop] = value
+    this.#s.syncDB?.send([
+      {
+        dataset: this.#dataset,
+        row: this.#id,
+        column: prop,
+        value: value,
+      },
+    ])
+    return true
+  }
+  deleteProperty(_: any, prop: string): any {
+    delete this.#s.mem[this.#dataset][this.#id][prop]
+    this.#s.syncDB?.send([
+      {
+        dataset: this.#dataset,
+        row: this.#id,
+        column: prop,
+        value: undefined,
+      },
+    ])
+  }
+  ownKeys() {
+    return Object.keys(this.#s.mem[this.#dataset][this.#id])
+  }
+  has(_: any, p: string) {
+    return p in this.#s.mem[this.#dataset][this.#id]
+  }
+  defineProperty(): any {
+    throw new TypeError('cannot defineProperty on store')
+  }
+  getOwnPropertyDescriptor(target: any, prop: string) {
+    const value = this.get(target, prop)
+    if (value) {
+      return {
+        value,
+        writable: true,
+        enumerable: true,
+        configurable: true,
+      }
+    }
+  }
+}
+
+class DatasetProxy {
+  #s: S<any>
+  #dataset: string
+  constructor(s: S<any>, dataset: string) {
+    this.#s = s
+    this.#dataset = dataset
+  }
+  get(_: any, id: string) {
+    const dataset = this.#s.mem?.[this.#dataset]
+    if (dataset && id in dataset) {
+      return new Proxy({}, new RowProxy(this.#s, this.#dataset, id))
+    }
+  }
+  set(_: any, id: string, value: object): any {
+    if (!this.#s.mem) {
+      throw new Error(
+        `cannot save data without logged in user in dataset "${
+          this.#dataset
+        }" with row id "${id}"`,
+      )
+    }
+    if (typeof value !== 'object') {
+      throw new Error(
+        `cannot use non object value in dataset "${
+          this.#dataset
+        }" with row id "${id}"`,
+      )
+    }
+
+    // work with a shallow clone
+    // TODO: consider deep clone?
+    value = { ...value }
+
+    // ensure we have an ID and it is what we expect
+    if ('id' in value) {
+      // @ts-expect-error id is special
+      if (id !== value.id) {
+        // @ts-expect-error id is special
+        const valueID = value.id
+        throw new Error(
+          `id mismatch in dataset "${
+            this.#dataset
+          }" with row id "${id}" and valud id ${valueID}`,
+        )
+      }
+    } else {
+      // @ts-expect-error id is special
+      value.id = id
+    }
+
+    const existing = this.#s.mem[this.#dataset]?.[id] ?? {}
+    this.#s.syncDB?.send(
+      // @ts-expect-error typescript doesn't understand filter
+      [
+        // update changed properties
+        ...Object.entries(value)
+          .map(([k, v]) => {
+            if (existing && deepEqual(existing[k], v)) {
+              return
+            }
+            return {
+              dataset: this.#dataset,
+              row: id,
+              column: k,
+              value: v,
+            }
+          })
+          .filter(v => v),
+        // drop missing properties
+        ...Object.keys(existing)
+          .map(k => {
+            if (k in value) {
+              return
+            }
+            return {
+              dataset: this.#dataset,
+              row: id,
+              column: k,
+              value: undefined,
+            }
+          })
+          .filter(v => v),
+      ],
+    )
+    let dataset = this.#s.mem[this.#dataset]
+    if (!dataset) {
+      dataset = this.#s.mem[this.#dataset] = {}
+    }
+    dataset[id] = value
+    return true
   }
   deleteProperty(): any {
     throw new TypeError('cannot delete on store')
   }
   ownKeys() {
-    return this.#s.knownDatasets()
+    const dataset = this.#s.mem[this.#dataset]
+    if (dataset) {
+      return Object.keys(dataset)
+    }
+    return []
   }
-  has(_: any, p: string) {
-    return this.#s.hasDataset(p)
+  has(_: any, id: string) {
+    const dataset = this.#s.mem[this.#dataset]
+    return dataset && !!dataset[id]
   }
   defineProperty(): any {
-    throw new TypeError('cannot defineProperty on store')
+    throw new TypeError('cannot defineProperty on dataset')
   }
-  getOwnPropertyDescriptor(_: any, p: string) {
-    return {
-      value: this.#s.dataset(p),
-      writable: true,
-      enumerable: true,
-      configurable: false,
+  getOwnPropertyDescriptor(target: any, id: string) {
+    const value = this.get(target, id)
+    if (value) {
+      return {
+        value,
+        writable: true,
+        enumerable: true,
+        configurable: true,
+      }
     }
   }
 }
@@ -70,8 +272,8 @@ class S<DB extends object> implements Store<DB> {
 
   // these exist if a user is signed in
   #idb?: IDBPDatabase
-  #syncDB?: SyncDB
-  #mem?: any
+  syncDB?: SyncDB
+  mem?: any
 
   constructor(opts: Opts) {
     this.#config = opts.config
@@ -85,10 +287,7 @@ class S<DB extends object> implements Store<DB> {
       })
     this.#auth.subscribe(this.#onAuthChange.bind(this), false)
 
-    this.#dbProxy = new Proxy(
-      Object.freeze({ name: 'rootStore' }),
-      new DBProxy(this),
-    )
+    this.#dbProxy = new Proxy({}, new DBProxy(this))
   }
 
   listenChanges() {
@@ -108,25 +307,25 @@ class S<DB extends object> implements Store<DB> {
 
   async #onAuthChange(user: User | undefined) {
     if (!user) {
-      this.#mem = undefined
+      this.mem = undefined
       this.#idb?.close()
       this.#idb = undefined
-      this.#syncDB = undefined
+      this.syncDB = undefined
       this.#datasetProxies = {}
       return
     }
 
-    this.#mem = {}
+    this.mem = {}
     this.#datasetProxies = {}
 
     const local = new LocalIndexedDB()
-    local.listenChanges(syncDatasetMem(this.#mem))
+    local.listenChanges(syncDatasetMem(this.mem))
     const dbName = this.#name ? `${this.#name}_${user.localId}` : user.localId
     this.#idb = await openDB(dbName, 1, {
       upgrade: db => local.upgradeDB(db),
       blocking: () => this.#idb?.close(),
     })
-    await loadDatasetMem(this.#mem, this.#idb)
+    await loadDatasetMem(this.mem, this.#idb)
     local.setDB(this.#idb)
 
     const groupID = this.#name ? `${user.localId}.${this.#name}` : user.localId
@@ -135,27 +334,17 @@ class S<DB extends object> implements Store<DB> {
       api: this.#api,
       groupID,
     })
-    this.#syncDB = await SyncDB.new(remote, local)
-  }
-
-  knownDatasets() {
-    return Object.keys(this.#mem)
+    this.syncDB = await SyncDB.new(remote, local)
   }
 
   hasDataset(dataset: string) {
-    return dataset in this.#mem
+    return dataset in this.mem
   }
 
-  dataset(dataset: string) {
+  datasetProxy(dataset: string) {
     let proxy = this.#datasetProxies[dataset]
     if (!proxy) {
-      // @ts-expect-error type bypass
-      proxy = new Proxy(Object.freeze({ name: dataset }), {
-        get(_, p) {
-          return null
-        },
-      })
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      proxy = new Proxy({}, new DatasetProxy(this, dataset))
       this.#datasetProxies[dataset] = proxy
     }
     return proxy
--- test/tests.ts
+++ test/tests.ts
@@ -6,13 +6,16 @@ import { initStore } from '../src/index.js'
 
 const userDaaku = 'daaku'
 const userShah = 'shah'
+const yoda = Object.freeze({ name: 'yoda', age: 942 })
 
 interface Jedi {
+  id?: string
   name: string
   age: number
 }
 
 interface Sith {
+  id?: string
   name: string
   convert: boolean
 }
@@ -73,8 +76,8 @@ QUnit.hooks.beforeEach(assert => {
 })
 
 QUnit.hooks.afterEach(async assert => {
-  await deleteDB(`${assert.id}_${userDaaku}`)
-  await deleteDB(`${assert.id}_${userShah}`)
+  //await deleteDB(`${assert.id}_${userDaaku}`)
+  //await deleteDB(`${assert.id}_${userShah}`)
 })
 
 QUnit.test('Test Logged Out', async assert => {
@@ -99,4 +102,8 @@ QUnit.test('Test Logged In', async assert => {
   assert.ok(store.db, 'db exists')
   assert.ok(store.db.jedi, 'collection exists')
   assert.notOk(store.db.jedi.yoda, 'objects dont exist')
+  store.db.jedi.yoda = yoda
+  assert.equal(store.db.jedi.yoda.name, yoda.name, 'expect yoda name')
+  assert.equal(store.db.jedi.yoda.age, yoda.age, 'expect yoda age')
+  assert.equal(store.db.jedi.yoda.id, 'yoda', 'expect yoda id')
 })
