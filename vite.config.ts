28211ebda32e1b50f57cf7fdc7bc701eff8a46ce
Naitik Shah <n@daaku.org>
2023-11-28 17:27:16 +0400

    enable sourcemap for test cover

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: './',
   build: {
+    sourcemap: true,
     rollupOptions: {
       input: resolve(__dirname, 'test/index.html'),
     },
